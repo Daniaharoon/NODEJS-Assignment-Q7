184b7c7d6936036115f762a08069d66c22010275
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:16:11 +0500

    Add files via upload

---
+++ ques7.js
@@ -0,0 +1,14 @@
+"use strict";
+//7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+let number = 2;
+let number2 = 6;
+console.log(number + number2);
+let num = 10;
+let num2 = 2;
+console.log(num - num2);
+let num3 = 2;
+let num4 = 4;
+console.log(num3 * num4);
+let num5 = 16;
+let num6 = 2;
+console.log(num5 / num6);
---
+++ ques7.ts
@@ -0,0 +1,17 @@
+//7. Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+let number : number = 2
+let number2 : number = 6
+console.log(number + number2);
+
+let num : number = 10
+let num2 : number = 2
+console.log(num - num2);
+
+let num3 : number = 2
+let num4 : number = 4
+console.log(num3 * num4);
+
+let num5 : number = 16
+let num6 : number = 2
+console.log(num5 / num6);
